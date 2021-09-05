13a547a70f1141a4cb22bf0e88116691ae09e9d8
nishibhate <64940059+nishibhate@users.noreply.github.com>
2021-09-05 21:08:01 +0530

    Add files via upload
    practical 2

---
+++ ScalarVectorAdd2.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>  
+#include <stdio.h>   
+#include <omp.h>     
+
+
+//shared - private
+int main (int argc, char *argv[]) 
+{
+	int * a;
+	int * c; 
+        
+    int n = 8;             
+	int n_per_thread;                 
+	int total_threads = 4;   
+	int i;     
+      
+    a = (int *) malloc(sizeof(int)*n);
+	c = (int *) malloc(sizeof(int)*n);
+
+    for(i=0; i<n; i++) 
+    {
+        a[i] = i;
+    }
+     
+        
+	omp_set_num_threads(total_threads);
+	
+	n_per_thread = n/total_threads;
+	
+	#pragma omp parallel for shared(a, c) private(i) 
+        for(i=0; i<n; i++) 
+        {
+			c[i] = a[i]+1;
+			printf("Thread %d works on element%d\n", omp_get_thread_num(), i);
+        }
+	
+	printf("Vector Addition :\n");
+	printf("i\ta[i]\t+\t1\t=\tc[i]\n");
+    for(i=0; i<n; i++) 
+    {
+		printf("%d\t%d\t\t%d\t\t%d\n", i, a[i], 1, c[i]);
+    }
+	
+    free(a);  
+    free(c);
+	
+	return 0;
+}
---
+++ VectorVectorAdd3.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>  
+#include <stdio.h>   
+#include <omp.h>     
+
+
+//firstprivate - shared - shared
+int main (int argc, char *argv[]) 
+{
+	int * a;
+	int * b; 
+	int * c;
+        
+    int n = 8;             
+	int n_per_thread;                 
+	int total_threads = 4;   
+	int i;     
+      
+    a = (int *) malloc(sizeof(int)*n);
+	b = (int *) malloc(sizeof(int)*n);
+	c = (int *) malloc(sizeof(int)*n);
+
+    for(i=0; i<n; i++) 
+    {
+        a[i] = i;
+    }
+    for(i=0; i<n; i++) 
+    {
+    	b[i] = i;
+    }   
+        
+	omp_set_num_threads(total_threads);
+	
+	n_per_thread = n/total_threads;
+	
+	#pragma omp parallel for firstprivate(b) shared(a, c) shared(i) 
+        for(i=0; i<n; i++) 
+        {
+			c[i] = a[i]+b[i];
+			printf("Thread %d works on element%d\n", omp_get_thread_num(), i);
+        }
+	
+	printf("Vector Addition :\n");
+	printf("i\ta[i]\t+\tb[i]\t=\tc[i]\n");
+    for(i=0; i<n; i++) 
+    {
+		printf("%d\t%d\t\t%d\t\t%d\n", i, a[i], b[i], c[i]);
+    }
+	
+    free(a);  
+    free(b); 
+    free(c);
+	
+	return 0;
+}
